07deb025df8813d0ad6b5d8eb51fa10c824b96cf
Hiroyuki Suminoue <suminoue.hiro@jp.fujitsu.com>
2017-08-25 18:06:14 +0900

    検索エリアのイメージ

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -7,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccordionComponent implements OnInit {
 
-  private accordions: Array<any> = [
-    { title: 'Dynamic Title 1', content: 'Dynamic content 1' },
-    { title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true },
-    { title: 'Dynamic Title 3', content: 'Dynamic content 3', active: true }
-  ];
+  // private accordions: Array<any> = [
+  //   { title: 'Dynamic Title 1', content: 'Dynamic content 1' },
+  //   { title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true },
+  //   { title: 'Dynamic Title 3', content: 'Dynamic content 3', active: true }
+  // ];
+  private accordionTitle = '検索エリア';
 
   multiple = false;
 
